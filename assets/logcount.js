d5f31e290235a704bb17c045b40c5113a60408de
0̴g <272173598+09-zg@users.noreply.github.com>
2026-04-04 21:07:34 +0900

    logcount.js を更新

--- assets/logcount.js
+++ assets/logcount.js
@@ -53,7 +53,7 @@ btn.style.opacity = '0.5';
 const count = await _send('like');
 
 // こう書くと、数字が取れたら「7✨」、ダメでも「✨」だけ表示されます
-btn.innerText = count ? `${count}✨` : '✨';
+btn.innerText = count ? `✨${count}` : '✨';
 
 /*
 btn.innerText = 'いいね！送信済み';
